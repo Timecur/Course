c35bae21ca1aeea711dd5fb11fc79826c218b305
nemo <m1157565335@163.com>
2020-02-13 22:33:14 +0800

    bulid project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <!-- 当前路由地址所显示的内容 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ src/pages/login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :style="bgImg">111</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      bgImg: {
+        backgroundImage: 'url(' + require('@/assets/login.jpg') + ')',
+        backgroundRepeat: 'no-repeat',
+				backgroundSize: '100% 100%',
+      }
+    }
+  }
+}
+</script>
